65a58d5a8095100d88bfd8e8ea7a65545b69d10c
HenrikOssipoff <henrik.ossipoff@gmail.com>
2020-06-09 22:05:59 +0200

    test and ci

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Roughly following https://medium.com/@vanflymen/blazing-fast-ci-with-github-actions-poetry-black-and-pytest-9e74299dd4a5
 
 See also this article about setting up self-hosted runners in k8s: https://sanderknape.com/2020/03/self-hosted-github-actions-runner-kubernetes/
+
+Notes on poetry for production with Docker: https://stackoverflow.com/questions/53835198/integrating-python-poetry-with-docker
--- githubactionstest/settings.py
+++ githubactionstest/settings.py
@@ -73,12 +73,20 @@ WSGI_APPLICATION = 'githubactionstest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.getenv("ISTEST", None):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
